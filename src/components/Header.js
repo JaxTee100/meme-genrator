f335d4b60da4a3e96c7e7d60d63122764b90c605
Udeh Tochukwu <tobytc0504@gmail.com>
2022-10-10 16:07:18 +0100

    memes added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () =>{
+    return(
+        <div className="header">
+            <h1>MEME</h1>
+            <h2>Get memes</h2>
+        </div>
+    )
+}
+
+export default Header;
